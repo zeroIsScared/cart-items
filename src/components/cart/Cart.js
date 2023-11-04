72fc33aeb7e16026ce089042d4e11280b89b0775
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-11-04 08:39:13 +0200

    removed unused imports

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,5 @@
 import CartItem from "./CartItem.js";
-import Product from "../product/Product.js";
-import Quantity from "../product/Quantity.js";
-import Image from "../product/Image.js";
-import Money from "../product/Money.js";
-import {getCartItems} from "./api.js"
-
-
+import {getCartItems} from "./api.js";
 
 const Cart = () => {
 
